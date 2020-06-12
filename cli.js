5f8366e09084a97f9d27253f44ced4654dfb5f87
Stanley Zapata <jrszapata@hotmail.com>
2020-06-11 21:31:59 -0400

    Refactor to remove deprecated dependencies. Add cors library

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const express = require("express");
-const request = require("request-promise");
+const axios = require("axios");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const usage = require("./help");
 const opts = require("./params");
@@ -19,41 +20,47 @@ if (!opts.to) {
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(async (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-
+  console.log(`\n\n============ REQUEST: ${req.method} ${req.url}`);
   const reqBody = {
-    method: req.method,
-    uri: opts.to + req.path,
-    json: true,
+    method: req.method.toLowerCase(),
+    url: opts.to + req.url,
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+    },
   };
 
   if (req.headers.authorization) {
-    reqBody.headers = {
-      authorization: req.headers.authorization,
-    };
+    reqBody.headers.authorization = req.headers.authorization;
   }
 
   if (req.body) {
-    reqBody.body = req.body;
+    console.log("req.body", req.body);
+    reqBody.data = req.body;
   }
 
-  console.log("Request: ", reqBody);
+  // console.log("Request: ...");
+  console.log("\n==> Request: ", reqBody);
 
   try {
-    const response = await request(reqBody);
-    console.log("Response:", response);
-    res.json(response);
+    const response = await axios(reqBody);
+    console.log("\n==> Response Success: Code " + response.status);
+    console.log(response.data ? response.data : "No response data");
+    // res.set(response.headers);
+    res.json(response.data);
+    res.status(response.status);
   } catch (err) {
-    const respError = err && err.response ? err.response : err;
-    console.log("Response Error:", respError);
-    res.status(err.statusCode);
-    res.json(respError);
+    const respError = err && err.response && err.response.data;
+    //     ? err.response.data
+    // console.log("\n==> Response Error: Code ", respError.response);
+    console.log("\n", respError);
+    // res.status(err.statusCode);
+    res.send(err);
   }
 });
 
